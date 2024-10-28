acfe4e9cdeb86c0c6fff71452c9005db56cc5060
Nishant Sushmakar <nishantsushmakar@Nishants-MacBook-Pro.local>
2024-10-28 23:43:08 +0100

    fix for 1st query

--- tpcdskit-official-mac/Queries/1GB/Optimized_Queries.sql
+++ tpcdskit-official-mac/Queries/1GB/Optimized_Queries.sql
@@ -1,3 +1,28 @@
+-- start query 1 in stream 0 using template query1.tpl
+with customer_total_return as
+(select sr_customer_sk as ctr_customer_sk
+,sr_store_sk as ctr_store_sk
+,sum(SR_FEE) as ctr_total_return
+from store_returns
+,date_dim
+where sr_returned_date_sk = d_date_sk
+and d_year =2000
+group by sr_customer_sk
+,sr_store_sk),
+store_average as (
+	select ctr_store_sk, avg(ctr_total_return)*1.2 as store_threshold
+	from customer_total_return
+	group by ctr_store_sk
+)
+select c_customer_id
+from customer_total_return ctr1,store,customer,store_average sa
+where ctr1.ctr_store_sk = sa.ctr_store_sk
+and ctr1.ctr_total_return > sa.store_threshold
+and s_store_sk = ctr1.ctr_store_sk
+and s_state = 'TN'
+and ctr1.ctr_customer_sk = c_customer_sk
+order by c_customer_id
+-- end query 1 in stream 0 using template query1.tpl
 -- start query 4 in stream 0 using template query4.tpl
 with year_total_s as (
  select c_customer_id customer_id
